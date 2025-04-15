37c82a7d7fa5a5ceae66c6e4339d59146cc95674
Tenshi . <jrcontreras446@gmail.com>
2025-04-14 23:20:35 -0600

    started with the store main page.

--- php/user_page.php
+++ php/user_page.php
@@ -8,21 +8,33 @@ if (!isset($_SESSION['email'])) {
 
 ?>
 
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Usuario</title>
+  <title>Amerian Style</title>
   <link rel="stylesheet" href="../css/main_page.css">
+  <link
+    href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css"
+    rel="stylesheet"
+/>
 </head>
 <body>
-  <div class="box">
-    <h1>Welcome, <span><?= $_SESSION['name']; ?></span></h1>
-    <p>This is an <span>User</span> page</p>
-    <button onclick="window.location.href='logout.php'"  >logout</button>
-  </div>
+
+  <nav>
+    <a href="user_page.php" class="logo" style="margin-right: auto;">Amerian Style.</a>
+
+    <a href="cart.php" class="cart-icon">
+      <i class="ri-shopping-bag-4-line"></i>
+      <span class="cart-item-count"></span>
+    </a>
+    <button onclick="window.location.href='logout.php'">logout</button>
+  </nav>
+
+
+
+
+
 </body>
 </html>
