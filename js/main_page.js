fae0b3913615beb527db3ce9ef295e6485a9084f
Tenshi . <jrcontreras446@gmail.com>
2025-04-16 18:14:30 -0600

    done with listing product, stable build

---
+++ js/main_page.js
@@ -0,0 +1,29 @@
+
+console.log("Hola, mundo!");
+const productContainer = document.querySelector(".product-list");
+
+
+if  (productContainer) {
+  displayProduct();
+}
+
+function displayProduct() {
+  products.forEach(product => {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+    productCard.innerHTML = `
+      <div class="img-box">
+        <img src="${product.colors[0].mainImage}">
+      </div>
+      <h2 class="tittle">${product.title}</h2>
+      <span class="price">${product.price}</span>
+    `;  
+    productContainer.appendChild(productCard);
+
+    const imgBox = productCard.querySelector(".img-box");
+    imgBox.addEventListener("click", () => {
+      sessionStorage.setItem("selectedProduct", JSON.stringify(product));
+      window.location.href = product.detail.html
+    });
+  });
+}
--- user_page.php
+++ user_page.php
@@ -42,7 +42,7 @@ if (!isset($_SESSION['email'])) {
 
 
 
-
+  <script src="js/products.js"></script>
   <script src="js/main_page.js"></script>
 </body>
 </html>
